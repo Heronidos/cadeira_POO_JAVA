59a73f504ec80974c80e612369ee1f3669f3e976
Heron <heronverissimo4@gmail.com>
2019-08-15 18:25:27 -0300

    s02e01

---
+++ caelum/src/s02e01/MeuPrograma.java
@@ -0,0 +1,12 @@
+package s02e01;
+
+class MeuPrograma {
+	public static void main(String[] args) {
+		System.out.println("Minha primeira aplicação Java!");
+	}
+}
+class Main {
+	public static void main(String[] args) {
+		System.out.println("Minha primeira aplicação Java!");
+	}
+}
---
+++ caelum/src/s02e01/caelum_cap_3/Ex6.java
@@ -0,0 +1,19 @@
+//0 1 1 2 3 5 8 13 21 34 55 89 144
+
+package s02e01.caelum_cap_3;
+public class Ex6 {
+    public static void main(String[] args){
+        int n = 0;
+        int n2 = 1;
+        int aux = 0;
+        while (n <= 200){
+            System.out.println(n);
+            aux = n;
+            n = n2;
+            n2 = aux + n;
+            
+        }
+        
+    }
+    
+}
